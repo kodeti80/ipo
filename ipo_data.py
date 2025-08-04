ff0bdf45e86146940959bc299ea841e36daa9631
kodeti80 <kodetimahesh786@gmail.com>
2025-08-04 18:21:51 +0530

    ipo_data.py

---
+++ ipo_data.py
@@ -0,0 +1,16 @@
+ipo_details = {
+    "Tata Technologies": {
+        "price": "₹475 - ₹500",
+        "issue_dates": "Aug 5 - Aug 7, 2025",
+        "lot_size": 30,
+        "status": "Not Allotted Yet",
+        "listing_date": "Aug 14, 2025"
+    },
+     "Bharti Hexacom": {
+        "price": "₹520 - ₹570",
+        "issue_dates": "July 10 - July 12, 2025",
+        "lot_size": 20,
+        "status": "Allotment Completed",
+        "listing_date": "July 20, 2025"
+    }
+}
